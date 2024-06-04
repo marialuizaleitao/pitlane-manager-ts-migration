8fbc8f6ec5e61c7157e392d94619f22392fdaa7e
Maria Leitão <malufln.99@gmail.com>
2024-06-04 15:51:24 -0300

    Clean up initial CRA files and comments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Banner from "./components/Banner/Banner";
 
 function App() {
   return (
     <div className="App">
-      <Banner>
-        <h1>Custom Banner</h1>
-      </Banner>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Banner />
     </div>
   );
 }
