a99dbc036a865ae0a16425644c5a00d4ea4521e8
Maria Leitão <malufln.99@gmail.com>
2024-06-05 15:47:30 -0300

    Implemented personnel addition functionality in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import Banner from "./components/Banner";
 import Form from "./components/Form";
 
 function App() {
+
+  const [staff, setStaff] = useState([])
+
+  const onAddedPersonnel = (personnel) => {
+    setStaff([...staff, personnel]);
+  } 
+
   return (
     <div className="App">
       <Banner />
-      <Form />
+      <Form
+        onPersonnelRegistered={(personnel) => onAddedPersonnel(personnel)}
+      />
     </div>
   );
 }
