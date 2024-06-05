f81c1041f7eb1567d30288c8baffe41e5853c5a7
Maria Leitão <malufln.99@gmail.com>
2024-06-05 17:24:23 -0300

    Styling Team section

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -2,8 +2,8 @@ import "./styles.css";
 
 const Team = (team) => {
   return (
-    <section className="team">
-      <h3>{team.name}</h3>
+    <section className="team" style={{ backgroundColor: team.secondaryColor }}>
+      <h3 style={{ borderColor: team.primaryColor }}>{team.name}</h3>
     </section>
   );
 };
